213ff065576eeeee8dc6aa945d9e39d6215d23f8
scaminom <jcamino7758@uta.edu.ec>
2024-05-25 12:45:10 -0500

    Test 3s in sleep initialization

--- src/users/infraestructure/users.module.ts
+++ src/users/infraestructure/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/shared/infraestructure/prisma/prisma.module';
 import { UserController } from './adapters/user.controller';
 import { UserService } from '../application/services/user.service';
+import { UserRepository } from './adapters/user.repository';
 
 @Module({
   controllers: [UserController],
@@ -9,6 +10,10 @@ import { UserService } from '../application/services/user.service';
     {
       provide: 'UserServiceInterface',
       useClass: UserService,
+    },
+    {
+      provide: 'UserRepositoryInterface',
+      useClass: UserRepository,
     },
   ],
   imports: [PrismaModule],
