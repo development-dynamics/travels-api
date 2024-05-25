aa75db1a4670363c27c01c77ec36144559e5f279
scaminom <jcamino7758@uta.edu.ec>
2024-05-25 11:48:42 -0500

    Test build app action

--- src/users/infraestructure/users.module.ts
+++ src/users/infraestructure/users.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from 'src/shared/infraestructure/prisma/prisma.module';
 import { UserController } from './adapters/user.controller';
 import { UserService } from '../application/services/user.service';
 import { UserRepository } from './adapters/user.repository';
