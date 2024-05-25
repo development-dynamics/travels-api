eff8e8ba23411978ff8e5a76942b57eac674ded6
devjaes <merajair17@gmail.com>
2024-05-25 15:43:26 -0500

    Update eslint

--- src/users/application/ports/inbound/user.service.interface.ts
+++ src/users/application/ports/inbound/user.service.interface.ts
@@ -1,11 +1,11 @@
-import { User } from '../../../domain/models/user.entity';
-import { CreateUserDto } from '../../dto/create-user.dto';
-import { UpdateUserDto } from '../../dto/update-user.dto';
+import { User } from '../../../domain/models/user.entity'
+import { CreateUserDto } from '../../dto/create-user.dto'
+import { UpdateUserDto } from '../../dto/update-user.dto'
 
 export interface UserServiceInterface {
-  createUser(user: CreateUserDto): Promise<User>;
-  getUserById(id: number): Promise<User>;
-  getUsers(): Promise<User[]>;
-  updateUser(id: number, user: UpdateUserDto): Promise<User>;
-  deleteUser(id: number): Promise<void>;
+  createUser(user: CreateUserDto): Promise<User>
+  getUserById(id: number): Promise<User>
+  getUsers(): Promise<User[]>
+  updateUser(id: number, user: UpdateUserDto): Promise<User>
+  deleteUser(id: number): Promise<void>
 }
