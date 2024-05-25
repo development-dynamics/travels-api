48bbde40768c6b62dd6974a66da6af30147583d8
scaminom <jcamino7758@uta.edu.ec>
2024-05-25 16:07:26 -0500

    Add public metatag to app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common'
+import { Public } from './auth/strategies/public.strategy'
 
 @Controller()
 export class AppController {
+  @Public()
   @Get()
   getHello(): string {
     return 'Hello World!'
