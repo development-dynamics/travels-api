ba1446c4a04c987d8e67e7688e8dead299d3e5a6
devjaes <merajair17@gmail.com>
2024-06-02 13:01:34 -0500

    Merge with feat/reservation-module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import { ValidationPipe } from '@nestjs/common'
 import * as session from 'express-session'
-import * as process from 'process'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { ValidationPipe } from '@nestjs/common'
 
@@ -37,25 +34,5 @@ async function bootstrap() {
       cookie: { maxAge: +3600000 },
     }),
   )
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  )
-  const config = new DocumentBuilder()
-    .setTitle('Travels API')
-    .setDescription('The Travels API description')
-    .setVersion('1.0')
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-
-  SwaggerModule.setup('api', app, document)
-  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
