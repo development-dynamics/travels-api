d8f0834a192882bdb70502cfb2908e7e4654e120
devjaes <merajair17@gmail.com>
2024-06-02 12:54:59 -0500

    Merge

--- src/clients/infraestructure/adapters/client.repository.ts
+++ src/clients/infraestructure/adapters/client.repository.ts
@@ -1,23 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common'
 import { ClientRepositoryInterface } from '../../application/ports/outbound/client.repository.interface'
 import { Client } from '../../domain/models/client.entity'
-import { PrismaService } from 'src/shared/infraestructure/prisma/prisma.service'
-import { ENHANCED_PRISMA } from '@zenstackhq/server/nestjs'
-import { enhance } from '@zenstackhq/runtime'
-import { ClsService } from 'nestjs-cls'
-import { PrismaClient } from '@prisma/client'
+import { EnhancedPrismaService } from 'src/shared/infraestructure/prisma/prisma.enhance.service'
 
 @Injectable()
 export class ClientRepository implements ClientRepositoryInterface {
   constructor(
-    @Inject(ENHANCED_PRISMA)
-    private readonly prisma: PrismaService,
-    private readonly clsService: ClsService,
+    @Inject('EnhancedPrismaService')
+    private readonly db: EnhancedPrismaService,
   ) {}
-
-  private get enhancedPrisma() {
-    return enhance(this.prisma, { user: this.clsService.get('auth') })
-  }
 
   async findAll(): Promise<Client[]> {
     return await this.db.enhancedPrisma.client.findMany()
@@ -38,7 +29,7 @@ export class ClientRepository implements ClientRepositoryInterface {
   }
 
   async update(id: number, client: Client): Promise<Client> {
-    return await this.enhancedPrisma.client.update({
+    return await this.db.enhancedPrisma.client.update({
       where: { id },
       data: client,
     })
